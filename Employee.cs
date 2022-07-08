a56f045325f3fad5bd3ee6d9e40a6ad68e95bfac
MaazMustafa <108923279+MaazMustafa@users.noreply.github.com>
2022-07-08 15:53:26 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ORMFrameWork.Models
+{
+    [Table("emp10")]
+    public class Employee
+    {
+       
+        [Key]
+        
+        public int empno { get; set; }
+        [Required]
+        public string ename { get; set; }
+        
+        //public DateTime hdate { get; set; }
+        [Required]
+        public string job { get; set; }
+        [Range(minimum:1,maximum:1000)]
+        public int deptno { get; set; }
+
+    }
+}
