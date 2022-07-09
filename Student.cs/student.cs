202e56880be363a140c92e4f733d6e494ebc7d40
mrunalmandhare26 <mrunalmandhare78@gmail.com>
2022-07-09 16:32:16 +0530

    chnages done & Constuctor added

--- Student.cs/student.cs
+++ Student.cs/student.cs
@@ -5,7 +5,26 @@ namespace name
 {
     public class Students
     {
-        public string StudentNo { get; set; }
+        public int StudentNo { get; set; }
         public string SName { get; set; }
+
+
+        // changes by Mru . . .
+        public int PRN { get; set; }
+        public int Age { get; set; }
+
+        public Students()
+        {
+
+        }
+        public Students(int rollno,string name,int prn.int age)
+        {
+            this.StudentNo = rollno;
+            this.SName = name;
+            this.PRN = prn;
+            this.Age = age;
+
+
+        }
     }
 }
