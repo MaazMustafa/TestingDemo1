075f5fc55e63efc06167b5249d6796b63eacd07f
MaazMustafa <maaz.234.mustafa@gmail.com>
2022-07-09 16:25:28 +0530

    Changes by Maaz

---
+++ Student.cs/student.cs
@@ -0,0 +1,11 @@
+using Ststem;
+using System.Collections.Generic;
+
+namespace name
+{
+    public class Students
+    {
+        public string StudentNo { get; set; }
+        public string SName { get; set; }
+    }
+}
